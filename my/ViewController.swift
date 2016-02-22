065af983dbb667f17aa42bccec2a1fe5c0887771
shradhariti <mobility@Mobilitys-Mac-mini.local>
2016-02-22 09:26:04 +0530

    Initial Commit

---
+++ my/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  my
+//
+//  Created by Mobility on 22/02/16.
+//  Copyright (c) 2016 Birlasoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
