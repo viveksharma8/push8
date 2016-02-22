77034c3f6e0d3811560fa91c57d4955abb61d960
shradhariti <mobility@Mobilitys-Mac-mini.local>
2016-02-22 09:41:59 +0530

    bkgrnd

--- my/ViewController.swift
+++ my/ViewController.swift
@@ -13,9 +13,11 @@ class ViewController: UIViewController {
     @IBOutlet var gitBtn: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = UIColor.blueColor()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
